49b0fb44725365fd58a8a0cecd603db83cb5882e
AMOGHPTL <amoghp549@gmail.com>
2026-03-05 16:42:41 +0530

    adding supply functionality

---
+++ frontend/src/pages/SupplyPage.tsx
@@ -0,0 +1,122 @@
+import { useNavigate, useParams } from "react-router-dom";
+import arrow from "../assets/icons/arrow-down-3101.svg";
+import { shortenHash, getReverseTokens } from "../utils/utils.js";
+import Tokens from "../abi/tokenToAddress.json";
+import syphonAddresses from "../abi/SyphonAddresses.json";
+import {
+  useGetMarketInfo,
+  useGetMarketParams,
+  useSupply,
+} from "../hooks/Syphon.js";
+import { useGetBorrowRate } from "../hooks/Irm.js";
+import { useChainId } from "wagmi";
+import Input from "../components/Input.js";
+import { useEffect, useState } from "react";
+import { formatEther } from "viem";
+
+const SupplyPage = () => {
+  const [supplyAmount, setSupplyAmount] = useState<bigint>(0n);
+
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+
+  const addressToToken = getReverseTokens(Tokens);
+  const chainId = useChainId();
+
+  const contractAddresses = syphonAddresses as Record<number, string>;
+  const syphonAddress = contractAddresses[chainId];
+
+  const { supply, isSuccess } = useSupply(syphonAddress);
+
+  const {
+    marketInfo,
+    isLoading: isLoadingMarketInfo,
+    error: errorMarketInfo,
+  } = useGetMarketInfo(syphonAddress, id);
+
+  const {
+    marketParams,
+    isLoading: isLoadingMarketParams,
+    error: errorMarketParams,
+  } = useGetMarketParams(syphonAddress, id);
+
+  const {
+    borrowRate,
+    isLoading: isLoadingBorrowRate,
+    error: errorBorrowRate,
+  } = useGetBorrowRate(marketParams?.irm, marketInfo);
+
+  if (!id) return <div>Invalid market</div>;
+
+  if (isLoadingMarketInfo || isLoadingMarketParams || isLoadingBorrowRate) {
+    return <div>.....Loading</div>;
+  }
+
+  if (errorMarketInfo || errorMarketParams || errorBorrowRate) {
+    return (
+      <div>
+        {errorMarketInfo && <p>Market info error: {errorMarketInfo.message}</p>}
+        {errorMarketParams && (
+          <p>Market params error: {errorMarketParams.message}</p>
+        )}
+        {errorBorrowRate && <p>Borrow rate error: {errorBorrowRate.message}</p>}
+      </div>
+    );
+  }
+
+  if (!marketInfo || !marketParams) {
+    return <div>No market data found.</div>;
+  }
+
+  const tokenSymbol = addressToToken[marketParams.loanToken];
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(`/markets/market/${id}`);
+    }
+  }, [isSuccess]);
+
+  return (
+    <div className="flex flex-col gap-[48px]">
+      <div
+        onClick={() => navigate(`/markets/market/${id}`)}
+        className="flex items-center cursor-pointer gap-[8px]"
+      >
+        <img src={arrow} alt="" className="w-[12px] rotate-90" />
+        <p className="text-[14px] text-gray-400">Market {shortenHash(id)}</p>
+      </div>
+
+      <div className="flex flex-col gap-[48px]">
+        <p className="text-[32px]">Supply Liquidity</p>
+
+        <div className="flex flex-col gap-[24px] p-[24px] bg-[#1d1c28] rounded-2xl w-[600px]">
+          <div className="flex items-center justify-between">
+            <p className="text-[24px]">Supply Liquidity {tokenSymbol}</p>
+
+            <img
+              src={`/tokens/${tokenSymbol}.svg`}
+              alt=""
+              className="w-[28px]"
+            />
+          </div>
+
+          <Input
+            inputAmount={supplyAmount}
+            setInputAmount={setSupplyAmount}
+            token={marketParams.loanToken}
+          />
+
+          <button
+            disabled={supplyAmount === 0n}
+            onClick={() => supply(marketParams, id, supplyAmount)}
+            className="bg-blue-600 hover:bg-blue-700 transition p-[10px] rounded-xl disabled:bg-gray-600 disabled:cursor-not-allowed"
+          >
+            Supply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SupplyPage;
