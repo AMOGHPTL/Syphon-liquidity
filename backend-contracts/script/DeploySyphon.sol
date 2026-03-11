3d46c3a6392252de615326fe323a416f088453e8
AMOGHPTL <amoghp549@gmail.com>
2026-03-11 17:49:02 +0530

    changing market tokens

--- backend-contracts/script/DeploySyphon.sol
+++ backend-contracts/script/DeploySyphon.sol
@@ -10,72 +10,86 @@ import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {MarketParams} from "../src/interfaces/ISyphon.sol";
 
 contract DeploySyphon is Script {
-    ERC20Mock usdt;
+    ERC20Mock cbBTC;
     ERC20Mock usdc;
-    ERC20Mock dai;
-    ERC20Mock usde;
-    ERC20Mock tusd;
-    ERC20Mock rlusd;
-    ERC20Mock usdcrv;
+    ERC20Mock sUSDS;
+    ERC20Mock usdt;
+    ERC20Mock sUSDe;
     ERC20Mock pyusd;
+    ERC20Mock wBTC;
+    ERC20Mock wstETH;
     Syphon syphon;
     MockIrm mockIrm;
-    MockOracle mockOracle;
+    MockOracle market1mockOracle;
+    MockOracle market2mockOracle;
+    MockOracle market3mockOracle;
+    MockOracle market4mockOracle;
+    MockOracle market5mockOracle;
 
     function run()
         public
-        returns (
-            Syphon,
-            MockIrm,
-            MockOracle,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock,
-            ERC20Mock
-        )
+        returns (Syphon, ERC20Mock, ERC20Mock, ERC20Mock, ERC20Mock, ERC20Mock, ERC20Mock, ERC20Mock, ERC20Mock)
     {
         vm.startBroadcast();
         syphon = new Syphon(msg.sender);
         mockIrm = new MockIrm();
-        mockOracle = new MockOracle(1e18);
-        usdt = new ERC20Mock();
+        market1mockOracle = new MockOracle(69426e18);
+        market2mockOracle = new MockOracle(108e16);
+        market3mockOracle = new MockOracle(122e16);
+        market4mockOracle = new MockOracle(69265e18);
+        market5mockOracle = new MockOracle(2469e18);
+
+        cbBTC = new ERC20Mock();
         usdc = new ERC20Mock();
-        dai = new ERC20Mock();
-        usde = new ERC20Mock();
-        tusd = new ERC20Mock();
-        rlusd = new ERC20Mock();
-        usdcrv = new ERC20Mock();
+        sUSDS = new ERC20Mock();
+        usdt = new ERC20Mock();
+        sUSDe = new ERC20Mock();
         pyusd = new ERC20Mock();
-        bytes32 market1Id =
-            syphon.createId(MarketParams(address(usdt), address(usdc), address(mockOracle), address(mockIrm), 916e15));
+        wBTC = new ERC20Mock();
+        wstETH = new ERC20Mock();
 
-        bytes32 market2Id =
-            syphon.createId(MarketParams(address(dai), address(usde), address(mockOracle), address(mockIrm), 916e15));
-        bytes32 market3Id =
-            syphon.createId(MarketParams(address(tusd), address(rlusd), address(mockOracle), address(mockIrm), 916e15));
+        //market IDs
+
+        bytes32 market1Id = syphon.createId(
+            MarketParams(address(cbBTC), address(usdc), address(market1mockOracle), address(mockIrm), 860e15)
+        );
+
+        bytes32 market2Id = syphon.createId(
+            MarketParams(address(sUSDS), address(usdt), address(market2mockOracle), address(mockIrm), 833e15)
+        );
+        bytes32 market3Id = syphon.createId(
+            MarketParams(address(sUSDe), address(pyusd), address(market3mockOracle), address(mockIrm), 899e15)
+        );
         bytes32 market4Id = syphon.createId(
-            MarketParams(address(usdcrv), address(pyusd), address(mockOracle), address(mockIrm), 916e15)
+            MarketParams(address(wBTC), address(usdc), address(market4mockOracle), address(mockIrm), 940e15)
         );
 
+        bytes32 market5Id = syphon.createId(
+            MarketParams(address(wstETH), address(usdc), address(market5mockOracle), address(mockIrm), 860e15)
+        );
+
+        //markets
+
         syphon.createMarket(
-            MarketParams(address(usdt), address(usdc), address(mockOracle), address(mockIrm), 916e15), market1Id
+            MarketParams(address(cbBTC), address(usdc), address(market1mockOracle), address(mockIrm), 860e15), market1Id
         );
         syphon.createMarket(
-            MarketParams(address(dai), address(usde), address(mockOracle), address(mockIrm), 916e15), market2Id
+            MarketParams(address(sUSDS), address(usdt), address(market2mockOracle), address(mockIrm), 833e15), market2Id
         );
         syphon.createMarket(
-            MarketParams(address(tusd), address(rlusd), address(mockOracle), address(mockIrm), 916e15), market3Id
+            MarketParams(address(sUSDe), address(pyusd), address(market3mockOracle), address(mockIrm), 899e15),
+            market3Id
         );
         syphon.createMarket(
-            MarketParams(address(usdcrv), address(pyusd), address(mockOracle), address(mockIrm), 916e15), market4Id
+            MarketParams(address(wBTC), address(usdc), address(market4mockOracle), address(mockIrm), 940e15), market4Id
+        );
+        syphon.createMarket(
+            MarketParams(address(wstETH), address(usdc), address(market5mockOracle), address(mockIrm), 860e15),
+            market5Id
         );
 
         vm.stopBroadcast();
 
-        return (syphon, mockIrm, mockOracle, usdt, usdc, dai, usde, tusd, rlusd, usdcrv, pyusd);
+        return (syphon, cbBTC, usdc, sUSDS, usdt, sUSDe, pyusd, wBTC, wstETH);
     }
 }
